1fcfb168307c89dfe71253dceed91f4a25ddb788
cblopez001 <cherakye.lopez@gmail.com>
2024-08-17 22:42:39 -0400

    minor edits

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './pages/homePage';
 import AboutPage from './pages/aboutPage';
 import ContactPage from './pages/contactPage';
