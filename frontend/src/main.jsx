f44a40c805c013e1bd4bfc25037dd1725352c76b
cblopez001 <cherakye.lopez@gmail.com>
2024-08-17 21:27:28 -0400

    edits to main.jsx and index.css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './app.css';
+import './index.css';  // Import your main CSS file here
+import App from './App';
 
 ReactDOM.render(
-  <Router>
+  <React.StrictMode>
     <App />
-  </Router>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
